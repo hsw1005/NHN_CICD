3bf62e16a363c6a85c4938b60088e74e8b83f671
hsw_1005 <43718769+hsw1005@users.noreply.github.com>
2022-11-19 21:33:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,19 @@
 
 import os
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 podname = os.uname()[1]
 
 @app.route("/")
 def index():
     return " Container hsw | POD Working : " + podname + " | v=1\n"
 
-@app.route("/test")
-def helloworld():
-    return render_template('index.html')
+# @app.route("/test")
+# def helloworld():
+#     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
