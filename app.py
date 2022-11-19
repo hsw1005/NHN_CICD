0f9840f7a4ca5a911fd24b097b0ff998f72b74c4
hsw_1005 <43718769+hsw1005@users.noreply.github.com>
2022-11-19 17:02:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 #!/usr/local/bin/python3
 
 import os
-from flask import Flask
+from flask import Flask, render_template
+
+from werkzeug.utils import send_from_directory
 
 app = Flask(__name__)
 podname = os.uname()[1]
 
-@app.route("/")
+# @app.route("/")
+# def index():
+#     return " Container hsw | POD Working : " + podname + " | v=2\n"
+
+
+@app.route('/')
 def index():
-    return " Container hsw | POD Working : " + podname + " | v=2\n"
+    return render_template('test.html')
+
+
+@app.route('/static/<path:filepath>')
+def routeto():
+
+    return send_from_directory('static', filepath)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+
