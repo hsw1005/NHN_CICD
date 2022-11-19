102c07b3c7c2b3214b29077fc29d694a75a4ddb1
hsw_1005 <43718769+hsw1005@users.noreply.github.com>
2022-11-19 18:49:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ podname = os.uname()[1]
 
 @app.route("/")
 def index():
-    return render_template('test.html')
-    #return " Container hsw | POD Working : " + podname + " | v=1\n"
+    return " Container hsw | POD Working : " + podname + " | v=1\n"
+    #return render_template('test.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
